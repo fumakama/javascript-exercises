b26d24b8a3fefe2ad017cbae511fd4cd050ea0da
fumakama <77727552+fumakama@users.noreply.github.com>
2023-06-14 14:10:44 +0400

    Complete ex 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ((Number.isInteger(a) && Number.isInteger(b)) && ((a > 0) && (b > 0))) {
+        let sumResult = 0;
 
+        if (a < b) {
+            for (let i = a; i <= b; i++) {
+                sumResult += i;
+            }
+        } else if (a > b) {
+            for (let i = b; i <= a; i++) {
+                sumResult += i;
+            }
+        }
+        return sumResult;
+    } else {return 'ERROR'};
 };
 
 // Do not edit below this line
