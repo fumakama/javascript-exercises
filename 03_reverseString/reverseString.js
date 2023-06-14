00c0640a59f824a746f917f6622398f098f35524
fumakama <77727552+fumakama@users.noreply.github.com>
2023-06-14 12:10:50 +0400

    Complete ex 3 & ex 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let resultString = '';
+    for (let i = string.length - 1; i > -1; i--) {
+        resultString += string[i];
+    }
+    return resultString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
+    let array = arguments[0];
 
+    for (let i = 1; i < arguments.length; i++) {
+        let index = array.indexOf(arguments[i]);
+        if (index != -1) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
