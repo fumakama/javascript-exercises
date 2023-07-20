97b2f8d5c4b603f28a2e8098741d32e74f7e3a94
fumakama <77727552+fumakama@users.noreply.github.com>
2023-07-20 16:58:06 +0400

    Wrote a first version of ex08

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let proccessedStr = str.split(" ").join("").split(",").
+    join("").split(".").join("").toLowerCase();
+
+    let median = Math.floor(proccessedStr.length / 2);
+    for (let i = 0; i<= median; i++) {
+        if (proccessedStr[i] !== proccessedStr[proccessedStr.length - (i + 1)]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
