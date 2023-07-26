4b798eab086430d9c966086c0aebaf83304399be
fumakama <77727552+fumakama@users.noreply.github.com>
2023-07-26 21:31:21 +0400

    Complete ex10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let arr = [1, 1];
 
+    let num = +a;
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num === 0) {
+        return 0;
+    }
+    if (num === 1 || num === 2) {
+        return arr[num - 1];
+    } else {
+        while (arr.length < num) {
+            let next = arr[arr.length - 1] + arr[arr.length - 2];
+            arr.push(next);
+        }
+        return arr[arr.length - 1];
+    }
 };
 
 // Do not edit below this line
