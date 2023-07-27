e671407041084eae7199941b4c130335445629d9
fumakama <77727552+fumakama@users.noreply.github.com>
2023-07-27 15:24:29 +0400

    Complete ex12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldestPerson, currentPerson) => {
+        if (!Object.keys(currentPerson).includes('yearOfDeath')) {
+            const date = new Date()
+            currentPerson['yearOfDeath'] = date.getFullYear();
+        }
+        return (currentPerson.yearOfDeath - currentPerson.yearOfBirth) >
+        (oldestPerson.yearOfDeath - oldestPerson.yearOfBirth) ? 
+        currentPerson : oldestPerson;
+    }, arr[0])
 };
 
 // Do not edit below this line
